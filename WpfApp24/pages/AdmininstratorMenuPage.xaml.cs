6679051c17f7981aa2151505ac284b23f530df6b
Ксения Жаркова <ksusha10_2001@mail.ru>
2020-04-29 13:07:12 +0300

    создание страниц

---
+++ WpfApp24/pages/AdmininstratorMenuPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AdmininstratorMenuPage.xaml
+    /// </summary>
+    public partial class AdmininstratorMenuPage : Page
+    {
+        public AdmininstratorMenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Pages/MainPage.xaml", UriKind.Relative));
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void BtnCharityList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
